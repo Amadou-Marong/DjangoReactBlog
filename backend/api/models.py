4ffb2a6e8dec29f34841eb13ccf38de4e35c63cd
Amadou-Marong <marongamadou6@gmail.com>
2024-09-07 10:39:46 +0100

    added all models

--- backend/api/models.py
+++ backend/api/models.py
@@ -109,6 +109,56 @@ class Post(models.Model):
         if self.slug == '' or self.slug == None:
             self.slug = slugify(self.title) + '-' + shortuuid.uuid()[:2]
         super(Post, self).save(*args, **kwargs)
-
+    
+    def comment(self):
+        return Comment.objects.filter(post=self).order_by('-id')
     
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    comment = models.TextField()
+    reply = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.name}"
+    
+    class Meta:
+        verbose_name_plural = 'Comment'
+
+    
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.post.title}"
+    
+    class Meta:
+        verbose_name_plural = 'Bookmark'
+
+
+class Notification(models.Model):
+    NOTI_TYPE = (
+        ('Like', 'Like'),
+        ('Comment', 'Comment'),
+        ('Bookmark', 'Bookmark')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100, choices=NOTI_TYPE)
+    seen = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Notification'
+
+    def __str__(self):
+        if self.post:
+            return f"{self.type} - {self.post.title}"
+        else:
+            return 'Notification'
+    
